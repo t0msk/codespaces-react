d9d7419bd580bfed3ec0ff5019eb3353d595fb5e
t0msk <tomsk.slovak@gmail.com>
2023-01-15 22:13:05 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-600">
-          Hello world!
-        </h1>
-    </div>
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
