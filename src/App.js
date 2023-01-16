12798ebd73d614482f38fb030c6430d235bb5c99
Tomáš Sabo <tomsk.slovak@gmail.com>
2023-01-16 10:55:08 +0100

    fixed tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
+    <h1 className="text-3xl font-bold text-red-600">
       Hello world!
     </h1>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: [
-    "./src/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{html,js}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
