e2975eb07fd41a94c36d04174bfecb27f115359f
Tomáš Sabo <tomsk.slovak@gmail.com>
2023-01-24 10:17:20 +0100

    typescript final fix of init

--- src/reportWebVitals.tsx
+++ src/reportWebVitals.tsx
@@ -1,5 +1,5 @@
 // @ts-ignore
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
